6a861f4462d37c4c38cb51ad30f83209eed3bcf5
tudor-iacobuta <itudor.adrian@gmail.com>
2019-01-17 16:53:58 +0200

    View change between two pages index and registerform. Established javascript architecture and stratedy

---
+++ webapp/public/javascript/index.js
@@ -0,0 +1,5 @@
+document.getElementById('newForm').addEventListener('click',function(e){
+
+    e.preventDefault();
+    fetchHTMLPage('registerform',targetDiv);
+});
--- webapp/public/javascript/main.js
+++ webapp/public/javascript/main.js
@@ -1,16 +1,67 @@
-const content = document.querySelector('.content');
-fetch('views/index.html').then(function(response){
+const htmlDocumentsFetched = {
+    'index' : false,
+    'formdisplay' : false,
+    'landing' : false,
+    'login' : false,
+    'registerform' : false,
+    'statistics' : false,
+};
 
-    if(response.status !== 200){
+function fetchJsScript(resourceName){
+
+    jsResource = `/javascript/${resourceName}.js`;
+    fetch(jsResource).then(function (res) {
+
+        return res.text();
+
+    }).then((jsRes) => {
+
+        const indexScript = document.createElement('script');
+        indexScript.innerText = jsRes;
+        document.querySelector('body').appendChild(indexScript);
+    });
+}
+function fetchHTMLPage(resourceName, targetDiv) {
+
+    htmlResource = `/views/${resourceName}.html`;
+    fetch(htmlResource).then(function (res) {
+
+        if (res.status === 200) {
+
+            return res.text();
+        }
         console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
+            res.status);
         return;
-    }
-    return response.text();
-}).then(function(html){
 
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html,"text/html");
-    const indexContent = doc.querySelector('.wrapper');
-    content.appendChild(indexContent);
-})
+    }).then(function (htmlText) {
+
+        //Get document Parser to create the DOM for the fetched html page
+        const parser = new DOMParser();
+        const fetchedHTMLPage = parser.parseFromString(htmlText, "text/html");
+
+        //Append the links from the fetched html page because DOM Parser doesn't do that
+        const linksToFetch = fetchedHTMLPage.querySelectorAll('link');
+        linksToFetch.forEach((link) => {
+
+            document.head.appendChild(link);
+        });
+
+        //Get the inner div from the fetched page and its appended to the targetDiv
+        const indexView = fetchedHTMLPage.querySelector('#view');
+        targetDiv.innerHTML = indexView.innerHTML;
+
+        //Fetch the javaScript file
+        fetchJsScript(resourceName);
+    })
+}
+
+function init(){
+
+    
+    fetchHTMLPage('index',targetDiv);
+    htmlDocumentsFetched['index'] = true;
+}
+const targetDiv = document.querySelector('#displayView');
+init();
+
